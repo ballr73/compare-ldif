f02d851866b14e69b06ae18ddc7045decfe090b4
Robert Ball <robert.ball4@nhs.net>
2023-05-19 12:26:13 +0100

    import std for readability

--- main.cpp
+++ main.cpp
@@ -6,13 +6,15 @@
 #include <set>
 #include <sstream>
 
-// Function to split a string into tokens
-std::vector<std::string> tokenize(const std::string& str, char delimiter) {
-    std::vector<std::string> tokens;
-    std::string token;
-    std::istringstream tokenStream(str);
+using namespace std;
 
-    while (std::getline(tokenStream, token, delimiter)) {
+// Function to split a string into tokens
+vector<string> tokenize(const string& str, char delimiter) {
+    vector<string> tokens;
+    string token;
+    istringstream tokenStream(str);
+
+    while (getline(tokenStream, token, delimiter)) {
         tokens.push_back(token);
     }
 
@@ -20,18 +22,18 @@ std::vector<std::string> tokenize(const std::string& str, char delimiter) {
 }
 
 // Function to read LDIF file and extract entries with attributes
-std::map<std::string, std::map<std::string, std::set<std::string>>> extractEntries(const std::string& filename) {
-    std::ifstream file(filename);
-    std::map<std::string, std::map<std::string, std::set<std::string>>> entries;
-    std::string line, dn, attribute;
+map<string, map<string, set<string>>> extractEntries(const string& filename) {
+    ifstream file(filename);
+    map<string, map<string, set<string>>> entries;
+    string line, dn, attribute;
 
-    while (std::getline(file, line)) {
+    while (getline(file, line)) {
         if (line.substr(0, 4) == "dn: ") {
             dn = line.substr(4);
-        } else if (line.find(':') != std::string::npos) {
-            std::vector<std::string> tokens = tokenize(line, ':');
+        } else if (line.find(':') != string::npos) {
+            vector<string> tokens = tokenize(line, ':');
             attribute = tokens[0];
-            std::string value = tokens[1].substr(1); // Remove leading space after colon
+            string value = tokens[1].substr(1); // Remove leading space after colon
             entries[dn][attribute].insert(value);
         }
     }
@@ -41,11 +43,11 @@ std::map<std::string, std::map<std::string, std::set<std::string>>> extractEntri
 }
 
 // Function to compare LDIF files and identify differences in entries and attributes
-void compareLDIFFiles(const std::string& file1, const std::string& file2, const std::string& outputFilename) {
-    std::map<std::string, std::map<std::string, std::set<std::string>>> entries1 = extractEntries(file1);
-    std::map<std::string, std::map<std::string, std::set<std::string>>> entries2 = extractEntries(file2);
+void compareLDIFFiles(const string& file1, const string& file2, const string& outputFilename) {
+    map<string, map<string, set<string>>> entries1 = extractEntries(file1);
+    map<string, map<string, set<string>>> entries2 = extractEntries(file2);
 
-    std::ofstream outputFile(outputFilename);
+    ofstream outputFile(outputFilename);
 
     for (const auto& entry : entries1) {
         if (entries2.find(entry.first) != entries2.end()) {
@@ -53,8 +55,8 @@ void compareLDIFFiles(const std::string& file1, const std::string& file2, const 
             auto& attributes2 = entries2[entry.first];
 
             for (const auto& attribute : attributes1) {
-                const std::string& attributeName = attribute.first;
-                const std::set<std::string>& values1 = attribute.second;
+                const string& attributeName = attribute.first;
+                const set<string>& values1 = attribute.second;
 
                 if (attributes2.find(attributeName) == attributes2.end()) {
                     // Attribute not present in the second file, add it with the values
@@ -62,7 +64,7 @@ void compareLDIFFiles(const std::string& file1, const std::string& file2, const 
                     outputFile << "changetype: modify\n";
                     outputFile << "add: " << attributeName << "\n";
 
-                    for (const std::string& value : values1) {
+                    for (const string& value : values1) {
                         outputFile << attributeName << ": " << value << "\n";
                     }
 
@@ -70,7 +72,7 @@ void compareLDIFFiles(const std::string& file1, const std::string& file2, const 
                 } else {
                     auto& values2 = attributes2[attributeName];
 
-                    for (const std::string& value : values1) {
+                    for (const string& value : values1) {
                         if (values2.find(value) == values2.end()) {
                             // Value not present in the second file, add it
                             outputFile << "dn: " << entry.first << "\n";
@@ -111,12 +113,12 @@ void compareLDIFFiles(const std::string& file1, const std::string& file2, const 
             }
 
             for (const auto& attribute : attributes2) {
-                const std::string& attributeName = attribute.first;
-                const std::set<std::string>& values2 = attribute.second;
+                const string& attributeName = attribute.first;
+                const set<string>& values2 = attribute.second;
 
                 if (attributes1.find(attributeName) == attributes1.end()) {
                     // Attribute not present in the first file, delete it from the second file
-                    for (const std::string& value : values2) {
+                    for (const string& value : values2) {
                         outputFile << "dn: " << entry.first << "\n";
                         outputFile << "changetype: modify\n";
                         outputFile << "delete: " << attributeName << "\n";
@@ -133,9 +135,9 @@ void compareLDIFFiles(const std::string& file1, const std::string& file2, const 
             outputFile << "changetype: add\n";
 
             for (const auto& attribute : entry.second) {
-                const std::string& attributeName = attribute.first;
+                const string& attributeName = attribute.first;
 
-                for (const std::string& value : attribute.second) {
+                for (const string& value : attribute.second) {
                     outputFile << attributeName << ": " << value << "\n";
                 }
             }
@@ -155,18 +157,18 @@ void compareLDIFFiles(const std::string& file1, const std::string& file2, const 
 
     outputFile.close();
 
-    std::cout << "Changes saved to " << outputFilename << "\n";
+    cout << "Changes saved to " << outputFilename << "\n";
 }
 
 int main(int argc, char* argv[]) {
     if (argc < 4) {
-        std::cout << "Usage: ./compareldif <file1.ldif> <file2.ldif> <output.ldif>\n";
+        cout << "Usage: ./compareldif <file1.ldif> <file2.ldif> <output.ldif>\n";
         return 1;
     }
 
-    std::string file1 = argv[1];
-    std::string file2 = argv[2];
-    std::string outputFilename = argv[3];
+    string file1 = argv[1];
+    string file2 = argv[2];
+    string outputFilename = argv[3];
 
     compareLDIFFiles(file1, file2, outputFilename);
 
